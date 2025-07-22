5613cd68dac7f77526840c4db83d0be8ea9e3f49
buzzcomando <diholabob8@gmail.com>
2025-07-22 16:57:48 -0300

    hola.py

--- unidad_01/hola.py
+++ unidad_01/hola.py
@@ -1,6 +1,5 @@
 hello_world.py
 print("hola mundo!")
 mensaje="hola mundo!")
-print(mensaje)
-mensaje="curso de programación Python"
+print(mensaje)mensaje="curso de programación Python"
 print(mensaje)
